a8c39280bd96c407df5ca3f069444919cb251797
SamuelJeff <samueljefferson143@gmail.com>
2026-08-03 23:47:10 -0300

    primeiras configuracoes

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+require("dotenv").config();
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: process.env.DB_CLIENT,
+
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+
+    migrations: {
+      directory: "./src/database/migrations",
+    },
+  },
+
+};
---
+++ src/database/migrations/20260804022609_create_users.js
@@ -0,0 +1,21 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+
+    table.increments("id").primary();
+
+    table.string("nome").notNullable();
+
+    table.string("email").notNullable().unique();
+
+    table.string("senha").notNullable();
+
+    table.string("cargo").notNullable();
+
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
